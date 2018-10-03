9e8fe6442b21e00cb85d2cbc7f0ceeeaaa5415d1
levonaramyan <levonaramyan@outlook.com>
2018-10-03 15:21:54 +0400

    Homework_5_2

---
+++ Types_and_Operations/homework_5_2_read_lower_write_uppercase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// PROBLEM: read from consol a lowercase English letter
+//          and write its uppercase representation
+
+namespace homework_5_2_read_lower_write_uppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a lowercase english text, for conversion to uppercase" +
+                "\nIf you want to stop conversions and exit, just type any digit");
+            bool exit = false;
+
+            while (!exit)
+            {                
+                ConsoleKeyInfo consolKeyInfo = Console.ReadKey();
+                string key = consolKeyInfo.KeyChar.ToString();
+                Console.WriteLine($" {key.ToUpper()}");
+                exit = int.TryParse(key, out int i);
+            }
+
+            Console.WriteLine("press any key to exit from console");
+            Console.ReadKey();
+        }
+    }
+}
