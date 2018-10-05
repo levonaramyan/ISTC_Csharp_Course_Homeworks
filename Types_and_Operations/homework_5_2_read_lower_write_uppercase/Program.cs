8ef3c77664bafffe158ee33771547735be8ab0b2
levonaramyan <levonaramyan@outlook.com>
2018-10-05 12:10:08 +0400

    homework_5_2_minor_update when char is digit, do not calculates an upKey

--- Types_and_Operations/homework_5_2_read_lower_write_uppercase/Program.cs
+++ Types_and_Operations/homework_5_2_read_lower_write_uppercase/Program.cs
@@ -16,16 +16,20 @@ namespace homework_5_2_read_lower_write_uppercase
             Console.WriteLine("Please enter a lowercase english text, for conversion to uppercase" +
                 "\nIf you want to stop conversions and exit, just type any digit");
             bool exit = false;
-
+            int diffLowerUpper = 'A' - 'a';
+            
             while (!exit)
             {                
                 ConsoleKeyInfo consolKeyInfo = Console.ReadKey();
-                string key = consolKeyInfo.KeyChar.ToString();
-                Console.WriteLine($" {key.ToUpper()}");
-                exit = int.TryParse(key, out int i);
+                char key = consolKeyInfo.KeyChar;
+                char upKey = char.IsLetter(key) ? (char)(key + diffLowerUpper) : upKey = key ;
+                Console.WriteLine($" {upKey}");
+                exit = int.TryParse($"{key}", out int i);
             }
-
+            
             Console.WriteLine("press any key to exit from console");
+
+            // Console.WriteLine(Convert.ToString(-2,2)); // print the binary
             Console.ReadKey();
         }
     }
