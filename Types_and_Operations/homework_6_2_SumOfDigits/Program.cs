e87355c684de51801312d9c3f30c4c8d62a54ded
levonaramyan <levonaramyan@outlook.com>
2018-10-05 15:55:13 +0400

    Homework_6_2_updated added some simpler and less functional ways

--- Types_and_Operations/homework_6_2_SumOfDigits/Program.cs
+++ Types_and_Operations/homework_6_2_SumOfDigits/Program.cs
@@ -17,11 +17,26 @@ namespace homework_6_2_SumOfDigits
                       
             // Reading and adding digits, until you typed a non digit char
             Console.Write("Please enter a four digit number:\nNum = ");
-            do
-            {
-                dig = Console.ReadKey().KeyChar;
-                if (char.IsDigit(dig)) sumOfDigits += Convert.ToInt32($"{dig}");
-            } while (char.IsDigit(dig));
+
+            //******* Reading a key, while it is digit and adding it in sum *****
+            //do
+            //{
+            //    dig = Console.ReadKey().KeyChar;
+            //    if (char.IsDigit(dig)) sumOfDigits += Convert.ToInt32($"{dig}");
+            //} while (char.IsDigit(dig));
+
+
+            // ****** Reading a number from console and calculates the sum of its digits
+            //string myNum = Console.ReadLine();
+            //foreach (char i in myNum)
+            //{
+            //    sumOfDigits += Convert.ToInt32($"{dig}");
+            //}
+
+            // The simplest way to calculate a sum of digits of 4 digit integer
+            int my4DigitNum = 4563;
+            sumOfDigits += Convert.ToInt32($"{my4DigitNum}"[0]) + Convert.ToInt32($"{my4DigitNum}"[1])
+                + Convert.ToInt32($"{my4DigitNum}"[2]) + Convert.ToInt32($"{my4DigitNum}"[3]);
 
             Console.Write($"\nThe sum of digits is: {sumOfDigits}");
             Console.ReadKey();
