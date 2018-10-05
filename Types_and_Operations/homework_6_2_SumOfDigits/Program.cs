10a44b312bac7a240977f625ee6760eee801551b
levonaramyan <levonaramyan@outlook.com>
2018-10-05 15:38:02 +0400

    Homework_6_2_The Sum of Digits

---
+++ Types_and_Operations/homework_6_2_SumOfDigits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The program calculates the sum of digits, readed from console
+
+namespace homework_6_2_SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfDigits = 0;
+            char dig ;
+                      
+            // Reading and adding digits, until you typed a non digit char
+            Console.Write("Please enter a four digit number:\nNum = ");
+            do
+            {
+                dig = Console.ReadKey().KeyChar;
+                if (char.IsDigit(dig)) sumOfDigits += Convert.ToInt32($"{dig}");
+            } while (char.IsDigit(dig));
+
+            Console.Write($"\nThe sum of digits is: {sumOfDigits}");
+            Console.ReadKey();
+        }
+    }
+}
