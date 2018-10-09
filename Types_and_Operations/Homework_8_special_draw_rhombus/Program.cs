142edd4de32e3f8ec6cdf4d27f31254df8be4335
levonaramyan <levonaramyan@gmail.com>
2018-10-09 23:11:59 +0400

    Homework_8_spec_draw_a_rhombus

---
+++ Types_and_Operations/Homework_8_special_draw_rhombus/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// PROBLEM: Given the side a of a rhombus, draw a rhombus with '*' symbols with a given side = a.
+
+namespace Homework_8_special_draw_rhombus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type the length of the rhombus");
+            int a = int.Parse(Console.ReadLine()); // reading the side
+
+            // initializing the longest line of a rhombus
+            string rhombus = "";
+            for (int i = 1; i <= 2 * a - 1; i++)
+                rhombus = $"{rhombus}*";
+
+            // adding symmetrically one line below and one line above
+            for (int i = a - 1; i > 0; i--)
+            {
+                int b = 2 * i - 1;
+                int c = a - i;
+                string currentLine = "";
+
+                // Adding '*'-s in currrent line
+                for (int j = 1; j <= b; j++)
+                {
+                    currentLine = $"{currentLine}*";
+                }
+
+                // Adding spaces in current line
+                for (int j = 1; j <= c; j++)
+                {
+                    currentLine = $" {currentLine}";
+                }
+
+                // putting one currentLine below, and one above
+                rhombus = $"{currentLine}\n{rhombus}\n{currentLine}";
+            }
+
+            // Printing the rhombus
+            Console.WriteLine(rhombus);
+            Console.ReadKey();
+        }
+    }
+}
