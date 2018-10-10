581eabb04bd23e59855e0f12ea66e62a2bc93edf
levonaramyan <levonaramyan@gmail.com>
2018-10-10 16:46:06 +0400

    Homework_8_spec4_task15

---
+++ Types_and_Operations/Homework_8_spec4_task15_SevenTimesMore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Խնդիր_15: Եռանիշ թվի առաջին թվանշանը ջնջել։ Եթե ստացված թիվը բազմապատկվի 7-ով,
+//           կստացվի սկզբնական թիվը։ Գտնել այդպիսի թվերը։
+
+namespace Homework_8_spec4_task15_SevenTimesMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro
+            Console.WriteLine("Printing all of 3 Digit numbers, which are 7 times more, than the one without the first digit.");
+            Console.WriteLine(new String('-', 100));
+
+            // Checking for all 3 digit i-s, and printing the number, if it is the case.
+            int i = 100;
+            while (i < 1000)
+            {
+                Console.Write(((i % 100) * 7 == i) ? $"{i}\t" : "");
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Types_and_Operations/Homework_8_spec4_task15_SevenTimesMore/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Homework_8_spec4_task15_SevenTimesMore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Homework_8_spec4_task15_SevenTimesMore")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c8953b4-94e8-4706-ad01-50443ece6043")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
