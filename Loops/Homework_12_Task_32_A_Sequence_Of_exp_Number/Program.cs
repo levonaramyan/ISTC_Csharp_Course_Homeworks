41458bd72992165d8f43937759cc5cbd6f77be1e
levonaramyan <levonaramyan@gmail.com>
2018-10-21 01:15:08 +0400

    Task 32: The sequence of number e

---
+++ Loops/Homework_12_Task_32_A_Sequence_Of_exp_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Խնդիր_32: Տրված է N բնական թիվը։ Օգտագործելով մեկ ցիկլ հաշվել 1 + 1/(1!) + 1/(2!) + 1/(3!) + … + 1/(N!) գումարը
+// (N!-ով նշանակված է 1-ից N թվերի արտադրյալը։ Ստացված թիվը հանդիսանում է e հաստատունի մոտավոր արժեքը)։
+
+
+
+namespace Homework_12_Task_32_A_Sequence_Of_exp_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing and printing the result
+            Console.WriteLine(ApproxExp(1_000_000));
+            Console.ReadKey();
+        }
+
+        // Returns the sum of n elements of the sequence of the approximation of e.
+        static double ApproxExp(int n)
+        {
+            double elem = 1;
+            double sum = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                elem /= i;
+                sum += elem;
+            }
+
+            return sum;
+        }
+    }
+}
