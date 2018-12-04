a38301b3b35f9460ba46a5b436753480d40d25d0
levonaramyan <levonaramyan@gmail.com>
2018-12-04 17:06:58 +0400

    Homework 13: Generics

---
+++ OOP/Homework_13_Generics/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_13_Generics
+{
+    static class Converter
+    {
+        // The method safely converts any input of type U into type T
+        // if it is not possible to convert, return the default value of type T
+        public static T StringToType<T,U>(U s)
+        {
+            Type myType = typeof(T);
+            T res;
+            try
+            {
+                res = (T)Convert.ChangeType(s, myType);
+            }
+            catch
+            {
+                res = default;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ OOP/Homework_13_Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_13_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "50"; // initial value
+            byte b = Converter.StringToType<byte,string>(a); // conveted value
+
+            Console.WriteLine(b*2); // printing the result of an operation with b
+            Console.ReadKey();
+        }
+    }
+}
